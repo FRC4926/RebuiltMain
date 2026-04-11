40e79fb41f356b3d2e6a9e302397226121a015eb
FRC4926 <programmingteam@team4926.org>
2026-04-11 09:44:10 -0400

    Edited shooterConstants

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -123,12 +123,13 @@ public class ShooterConstants {
     public static final double manualRPM = 4200;
     public static final double manualAngle = 18.0;
 
+    //calculations
     public static final double speed1 = 3800;
     public static final double speed2 = 4500;
 
-    public static final double distance1 = 0;
-    public static final double distance2 = 18;
+    public static final double distance1 = 1.4;
+    public static final double distance2 = 4.765;
 
-    public static final double angle1 = 0;
-    public static final double angle2 = 25;
+    public static final double angle1 = 8;
+    public static final double angle2 = 21.25;
 }
